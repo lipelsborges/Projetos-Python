160ab91d0c2ce9f544f7c49c0bbcfeb7d8341298
Felipe Borges <lipelsborges@gmail.com>
2025-08-22 21:06:43 -0300

    Update MaiorNum.py

---
+++ python project/MaiorNum.py
@@ -0,0 +1,16 @@
+
+
+def maior_numero(a, b, c):
+    maior = a
+
+    if b > maior:
+        maior = b
+    if c > maior:
+        maior = c
+
+    return maior
+    
+    
+print(maior_numero(10,25,15))
+    
+    
