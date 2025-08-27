760ccb9ccd60a457cba7d3ded83c8acf45f5b5f4
Felipe Borges <lipelsborges@gmail.com>
2025-08-27 15:59:09 -0300

    Create praticas.py

---
+++ APIss/praticas.py
@@ -0,0 +1,17 @@
+import requests
+
+def buscar(tipo, nome):
+
+    if tipo == "artist":
+        url = f"https://api.deezer.com/search/artist?q={nome}"  
+    elif tipo == "album":
+        url = f"https://api.deezer.com/search/album?q={nome}"
+    else:
+        url = f"https://api.deezer.com/search?q={nome}"
+    
+    res = requests.get(url)
+    dados = res.json()
+
+    if "data" not in dados or len(dados["data"]) == 0:
+        print("Nenhum resultado encontrado.")
+    return
